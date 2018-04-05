13633e5cc1d00be11d92c461c8d00b22b41a93ba
Tomoyuki Sakurai <y@trombik.org>
2018-04-05 21:33:13 +0900

    sync package name

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -14,7 +14,7 @@ when "ubuntu"
 when "openbsd"
   additional_packages = ["vim-spell-uk"]
 when "freebsd"
-  package = "vim-lite"
+  package = "vim-console"
   additional_packages = ["shells/tcshrc"]
 end
 
